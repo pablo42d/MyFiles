fe59bcfae11bec7020bee27fbafd885e8d29bdf3
pablo42d <adresprywatny@interia.eu>
2025-05-31 20:47:39 +0200

    kal

---
+++ Study/Java/DOM.js
@@ -0,0 +1,6 @@
+// Manipulacja drzewem DOM
+// Pozwala na tworzenie, modyfikowanie i usuwanie elementów HTML i XML
+// DOM to interfejs programowania aplikacji (API) dla dokumentów HTML i XML
+// DOM jest reprezentacją struktury dokumentu jako drzewa obiektów
+// DOM jest niezależny od języka programowania, ale najczęściej używany z JavaScript
+
