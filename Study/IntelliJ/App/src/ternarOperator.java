b261b2a5f33dce1e75c803dbb7b9381c8195f9e4
pablo42d <adresprywatny@interia.eu>
2025-06-08 19:10:02 +0200

    ternar

--- Study/IntelliJ/App/src/ternarOperator.java
+++ Study/IntelliJ/App/src/ternarOperator.java
@@ -1,6 +1,24 @@
 public class ternarOperator {
     public static void main(String[] args) {
 
+
+        int age = 16;
+        String messageTernar = age >= 18 ? "Adult" : "Child";
+
+        System.out.println(messageTernar);
+
+
+//        ---------------------------------------------
+
+        String message;
+        if (age >= 18) {
+            message = "Adult";
+        }
+        else {
+            message = "Child";
+        }
+
+        System.out.println(message);
     }
 
 }
